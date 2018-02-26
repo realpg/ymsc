6e58a95125f0661b9f6f64fc470bf2424feb0807
RZZLUGLXHNDRCQU\Administrator <zm_happiness66@126.com>
2018-02-26 09:25:34 +0800

    by zm

--- resources/views/home/center/index.blade.php
+++ resources/views/home/center/index.blade.php
@@ -114,6 +114,7 @@
                             console.log(JSON.stringify(ret));
                             if (ret.result) {
                                 layer.msg(ret.msg, {icon: 1, time: 2000});
+                                window.location.reload()
                             } else {
                                 layer.msg(ret.msg, {icon: 2, time: 2000});
                             }
