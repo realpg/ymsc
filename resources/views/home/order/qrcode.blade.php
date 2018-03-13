0932a8682c4c85d99ca2065759f3b5bab72ae04b
RZZLUGLXHNDRCQU\Administrator <zm_happiness66@126.com>
2018-03-13 15:07:38 +0800

    by zm

--- resources/views/home/order/qrcode.blade.php
+++ resources/views/home/order/qrcode.blade.php
@@ -3,11 +3,10 @@
     <div id="main-body">
         <div class="style-home-nav-station"></div>
         @include('home.layouts.payProgress')
-        <div class="container margin-top-20">
+        <div class="container margin-top-20 text-center">
             @if($order['suborders'])
                 <div id="output"></div>
                 <h4>请打开微信扫一扫，扫描图中二维码进行支付</h4>
-                {{$order['code_url']}}
             @else
                 <div class="margin-top-20 margin-right-10 margin-left-10 text-center">
                     <img src="{{ URL::asset('img/nothing.png') }}"  />
