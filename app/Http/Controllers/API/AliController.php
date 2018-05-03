f36211321d030ce3ee3a2d8935e4ae86e63d7215
RZZLUGLXHNDRCQU\Administrator <zm_happiness66@126.com>
2018-05-03 09:46:43 +0800

    by Amy 调试支付宝支付异步通知

--- app/Http/Controllers/API/AliController.php
+++ app/Http/Controllers/API/AliController.php
@@ -29,7 +29,7 @@ class AliController extends Controller
         $config = [
             'appid' => Utils::ALIPAY_APPID, // APP APPID
             'notify_url' => Utils::ALIPAY_NOTIFY_URL,
-            'return_url' => Utils::ALIPAY_RETRUN_URL,
+//            'return_url' => Utils::ALIPAY_RETRUN_URL,
             'ali_public_key' => Utils::ALIPAY_PUBLIC_KEY,     // 支付宝公钥，1行填写
             'private_key' => Utils::ALIPAY_PRIVATE_KEY,        // 自己的私钥，1行填写
             'log' => [ // optional
@@ -39,49 +39,6 @@ class AliController extends Controller
         ];
         return $config;
     }
-
-    /*
-     * 支付宝支付成功回调（根据微信改编）
-     *
-     * By zm
-     *
-     * 2018-04-03
-     */
-//    public function aliNotify(Request $request)
-//    {
-//        $config = $this->getConfigForAli();
-//        $ali = Pay::alipay($config);
-//        $user=$request->cookie('user');
-//        try {
-//            $data = $ali->verify($request->getContent()); // 是的，验签就这么简单！
-//            Log::info('Ali notify', $data->all());
-//            $data=$data->alipay_trade_precreate_response;
-//            //支付成功
-//            if ($data->code == "10000") {
-//                //总订单out_trade_no
-//                $out_trade_no = $data->out_trade_no;
-//                Log::info('order out_trade_no:'.$data->out_trade_no);
-//                //针对总订单进行处理
-////                $order = OrderManager::getOrderByUserIdAndTradeNoWithoutSuborder($user['id'],$out_trade_no);
-//                $order=OrderModel::where('trade_no',$out_trade_no)->first();
-//                $order_data['pay_at']=date("Y-m-d H:i:s");
-//                $order_data['status']=2;
-//                $order=OrderManager::setOrder($order,$order_data);
-//                $reuslt=$order->save();     //总订单设定支付时间和订单状态
-//                if($reuslt){
-//                    Log::info('order trade_no:'.$order->trade_no);
-//                    //会员积分变更
-//                    $member=UserModel::find($order['user_id']);
-//                    $member_data['score']=$member['score']+floor($order['total_fee']/100);       //积分按元取整来算
-////                    $member_data['score']=$member['score']+$order['total_fee'];       //积分按分来算
-//                    $member=MemberManager::setUser($member,$member_data);
-//                    $member->save();
-//                }
-//            }
-//        } catch (Exception $e) {
-//            Log::info($e->getMessage());
-//        }
-//    }
     /*
      * 支付宝支付成功回调（根据文档写的）
      *
--- app/Http/Controllers/Home/OrderController.php
+++ app/Http/Controllers/Home/OrderController.php
@@ -57,7 +57,7 @@ class OrderController
         $config = [
             'app_id' => Utils::ALIPAY_APPID, // APP APPID
             'notify_url' => Utils::ALIPAY_NOTIFY_URL,
-            'return_url' => Utils::ALIPAY_RETRUN_URL,
+//            'return_url' => Utils::ALIPAY_RETRUN_URL,
             'ali_public_key' => Utils::ALIPAY_PUBLIC_KEY,     // 支付宝公钥，1行填写
             'private_key' => Utils::ALIPAY_PRIVATE_KEY,        // 自己的私钥，1行填写
             'log' => [ // optional
