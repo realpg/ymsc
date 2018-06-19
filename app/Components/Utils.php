ac3aae06c0c2f3d47593a833a6dc4ef623c9d56d
RZZLUGLXHNDRCQU\Administrator <zm_happiness66@126.com>
2018-06-19 13:09:05 +0800

    by Amy 调试微信登录

--- app/Components/Utils.php
+++ app/Components/Utils.php
@@ -56,7 +56,7 @@ class Utils
 
     //微信登录
     const WECHAT_LOGIN_APP_ID='wxe6deb72d8381e0f2';
-    const WECHAT_LOGIN_APP_SECRET='';
+    const WECHAT_LOGIN_APP_SECRET='81c5ffe6e266fa69a5d357e31ecfcc64';
     const WECHAT_LOGIN_REDIRECT_URL='http://umylab.com/api/wechatLogincallback';
 
 
