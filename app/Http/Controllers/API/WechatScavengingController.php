c730dce87d9ff0c38cd5fa8e2f60570770755ee5
RZZLUGLXHNDRCQU\Administrator <zm_happiness66@126.com>
2018-06-19 16:57:05 +0800

    by Amy

--- app/Http/Controllers/API/WechatScavengingController.php
+++ app/Http/Controllers/API/WechatScavengingController.php
@@ -52,7 +52,7 @@ class WechatScavengingController extends Controller
 //        dd($userinfo);
             if(array_key_exists('openid',$userinfo)){
                 $data['signInBinding']=$userinfo;
-                return redirect()->route('signInBinding', $data);
+                return redirect()->action('signInBinding', $data);
             }
             else{
 
